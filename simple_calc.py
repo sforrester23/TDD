0fd0dc7b2e67ac773d3ad7c6c0540249b7279edf
Sam Forrester <SForrester@spartaglobal.com>
2019-10-08 16:01:40 +0100

    Made the calculator file and ran the tests to return them passing

---
+++ simple_calc.py
@@ -0,0 +1,13 @@
+class SimpleCalc():
+
+    def add(self, arg1, arg2):
+        return arg1 + arg2
+
+    def subtract(self, arg1, arg2):
+        return arg1 - arg2
+
+    def multiply(self, arg1, arg2):
+        return arg1 * arg2
+
+    def divide(self, arg1, arg2):
+        return arg1/arg2
--- test_unittest_simplecalc.py
+++ test_unittest_simplecalc.py
@@ -7,12 +7,24 @@ class CalcTest(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.calc.add(2, 4), 6)
-        
+
+    def test_add2(self):
+        self.assertEqual(self.calc.add(99, 5), 104)
+
     def test_subtract(self):
         self.assertEqual(self.calc.subtract(6, 4), 2)
+
+    def test_subtract2(self):
+        self.assertEqual(self.calc.subtract(74, 5), 69)
 
     def test_multiply(self):
         self.assertEqual(self.calc.multiply(3, 4), 12)
 
+    def test_multiply2(self):
+        self.assertEqual(self.calc.multiply(17, 17), 289)
+
     def test_divide(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
+
+    def test_divide2(self):
+        self.assertEqual(self.calc.divide(529, 23), 23)
